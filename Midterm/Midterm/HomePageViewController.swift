e7574ee7d444030348ce71fd23e7818c545d571d
Willy <willyxiyu@gmail.com>
2021-10-12 17:36:58 +0800

    sending data check

--- Midterm/Midterm/HomePageViewController.swift
+++ Midterm/Midterm/HomePageViewController.swift
@@ -6,18 +6,26 @@
 //
 
 import UIKit
+import FirebaseFirestore
+
 
 class HomePageViewController: UIViewController {
     
     let tableView = UITableView()
     let publishArticlePageViewController = PublishArticlePageViewController()
     
-    
-    
+    var db: Firestore!
+    var publishArticleList = [[String: Any]]()
+    var articleTitle: [String] = []
+    var authorName: [String] = []
+    var createdTime: [Int] = []
+    var content: [String] = []
+    var category: [String] = []
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        db = Firestore.firestore()
         self.view.backgroundColor = .white
         title = "Publisher"
         setUpTableView()
@@ -26,16 +34,28 @@ class HomePageViewController: UIViewController {
         tableView.delegate = self
         setupPublishArticlePageButton()
         
+//        getPublishArticleList { articleTitle, authorName, createdTime, content, category in
+//            self.articleTitle.append(articleTitle)
+//
+//
+//
+//        }
+        
+        
+        tableView.reloadData()
+        
+        
         // Do any additional setup after loading the view.
     }
     
     lazy var publishArticlePageButton: UIButton = {
         let publishArticlePageButton = UIButton(type: .custom)
         publishArticlePageButton.translatesAutoresizingMaskIntoConstraints = false
-        publishArticlePageButton.setImage(UIImage(systemName: "add"), for: .normal)
+        publishArticlePageButton.setBackgroundImage(UIImage(systemName: "circle.fill"), for: .normal)
+        publishArticlePageButton.tintColor = .purple
         publishArticlePageButton.setTitle("+", for: .normal)
-        publishArticlePageButton.backgroundColor = .purple
-//        publishArticlePageButton.layer.cornerRadius 
+        publishArticlePageButton.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+        publishArticlePageButton.contentVerticalAlignment = .center
         publishArticlePageButton.isEnabled = true
         publishArticlePageButton.addTarget(self, action: #selector(addNewArticle), for: .touchUpInside)
         return publishArticlePageButton
@@ -45,8 +65,8 @@ class HomePageViewController: UIViewController {
     @objc private func addNewArticle() {
         navigationController?.pushViewController(publishArticlePageViewController, animated: true)
     }
-
-
+    
+    
     private func setUpTableView() {
         
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -63,14 +83,43 @@ class HomePageViewController: UIViewController {
         self.view.addSubview(publishArticlePageButton)
         NSLayoutConstraint.activate([
             publishArticlePageButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
-            publishArticlePageButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+            publishArticlePageButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            publishArticlePageButton.heightAnchor.constraint(equalToConstant: 60),
+            publishArticlePageButton.widthAnchor.constraint(equalToConstant: 60)
         ])
         
         
     }
     
+    func getPublishArticleList(handler: @escaping ([String], [String], [Int], [String], [String]) -> Void) {
+        let ref = db.collection("articles")
+        ref.whereField("name", isEqualTo: "AKA小安老師")
+            .getDocuments { snapshot, error in
+                
+                var articleTitle: [String] = []
+                var authorName: [String] = []
+                var createdTime: [Int] = []
+                var content: [String] = []
+                var category: [String] = []
+                
+                guard let snapshot = snapshot else { return }
+                let publishArticleList = snapshot.documents
+                publishArticleList.forEach { snapshot in
+                    snapshot.data()
+                    articleTitle.append(snapshot.data()["title"] as? String ?? "")
+                    authorName.append(snapshot.data()["name"] as? String ?? "")
+                    createdTime.append(snapshot.data()["createdTime"] as? Int ?? 0)
+                    content.append(snapshot.data()["content"] as? String ?? "")
+                    category.append(snapshot.data()["category"] as? String ?? "")
+                    handler(articleTitle, authorName, createdTime, content, category)
+                }
+                
+            }
+        
+    }
+    
+   
 }
-
 extension HomePageViewController: UITableViewDelegate {
     
 }
@@ -78,17 +127,18 @@ extension HomePageViewController: UITableViewDelegate {
 extension HomePageViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        
+        return articleTitle.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: "\(TableViewCell.self)"), for: indexPath) as? TableViewCell else { fatalError("Error") }
         
-        cell.categoryLabel.text = "category"
-        cell.nameLabel.text = "name"
-        cell.titleLabel.text = "title"
-        cell.contentLabel.text = "content"
-        cell.timeLabel.text = "time"
+        cell.categoryLabel.text = category[indexPath.row]
+        cell.nameLabel.text = authorName[indexPath.row]
+        cell.titleLabel.text = articleTitle[indexPath.row]
+        cell.contentLabel.text = content[indexPath.row]
+        cell.timeLabel.text = String("\(createdTime[indexPath.row])")
         
         
         return cell
@@ -97,6 +147,6 @@ extension HomePageViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 220
     }
-
+    
 }
 
--- Midterm/Midterm/PublishArticlePageViewController.swift
+++ Midterm/Midterm/PublishArticlePageViewController.swift
@@ -26,7 +26,6 @@ class PublishArticlePageViewController: UIViewController {
         setupInputCategoryTextField()
         setupInputContentTextField()
         setupInputbutton()
-        
         db = Firestore.firestore()
         
     }
@@ -77,7 +76,6 @@ class PublishArticlePageViewController: UIViewController {
         inputContentTextField.placeholder = "input content"
         return inputContentTextField
     }()
-    
     
     lazy var button: UIButton = {
         let button = UIButton()
@@ -138,14 +136,16 @@ class PublishArticlePageViewController: UIViewController {
     }
     
     func addData() {
-    let articles = Firestore.firestore().collection("articles")
-    let document = articles.document()
-    let data: [String: Any] = [
-    "email": "wayne@school.appworks.tw",
-//    "id": "waynechen323",
-    "name": "AKA小安老師",
+    let articles = db.collection("articles")
+        let document = articles.document()
+        let data: [String: Any] = [
+        "author": [
+        "email": "wayne@school.appworks.tw",
+        "id": "waynechen323",
+        "name": "AKA小安老師"
+        ],
     "title": "IU「亂穿」竟美出新境界！笑稱自己品味奇怪　網笑：靠顏值撐住女神氣場",
-    "content": "南韓歌手IU（李知恩）無論在歌唱方面或是近期的戲劇作品都有亮眼的成績，但俗話說人無完美、美玉微瑕，曾再跟工作人員的互動影片中坦言自己品味很奇怪，近日在IG上分享了宛如「媽媽們青春時代的玉女歌手」超復古穿搭造型，卻意外美出新境界。",
+            "content":"南韓歌手IU（李知恩）無論在歌唱方面或是近期的戲劇作品都有亮眼的成績，但俗話說人無完美、美玉微瑕，曾再跟工作人員的互動影片中坦言自己品味很奇怪，近日在IG上分享了宛如「媽媽們青春時代的玉女歌手」超復古穿搭造型，卻意外美出新境界。" ,
     "createdTime": NSDate().timeIntervalSince1970,
     "id": document.documentID,
     "category": "Beauty"
